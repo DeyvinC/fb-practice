951f1a0a23ad40ad92b0c84595124d3a3a60b297
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-01-27 10:58:38 -0500

    imported our restraunts.json

---
+++ index.js
@@ -0,0 +1,10 @@
+// import our restraunts 
+const restraunts = require('./restraunts.json');
+
+console.log(restraunts[0].Name)
+
+// connect to Firestore
+
+// create a collection called "restraunts"
+
+// add each restraunts
