ca61942b0372a1440b2e53749465ad5fd9dbc1f2
“Deyvin <deyvinvelasquez0202@gmail.com>
2022-01-27 12:07:54 -0500

    Added Restrauants

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 // import our restraunts 
 const restraunts = require('./restraunts.json');
 
-console.log(restraunts[0].Name)
+//import a set of tools to talk to firebase and firestore
 
-// connect to Firestore
+const { initializeApp, applicationDefault, cert} = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require ('firebase-admin/firestore');
+
+//import our credentials
+const credentials = require('./credentials.json');
+// const { Certificate } = require('crypto');
+
+// connect to Firestore services
+initializeApp({
+    credential: cert(credentials)
+});
+// connect to firestore 
+const db = getFirestore();
 
 // create a collection called "restraunts"
 
+
 // add each restraunts
+db.collection('restraunts').add(restraunts[1])
+ .then(doc => {
+     console.log('Added Restraunt', doc.id)
+ })
+ .catch(err => {console.error(err)
+});
